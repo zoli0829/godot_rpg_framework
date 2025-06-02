811cc9318dcb9ef955d21011affafc1e4bf70884
Zoltan Vegh <zoli0829@gmail.com>
2025-06-02 15:52:29 +0100

    started implementing the navigation mesh

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -10,6 +10,7 @@ class_name Enemy
 @onready var collision_shape_3d: CollisionShape3D = $CollisionShape3D
 @onready var player_detector: ShapeCast3D = $Rig/PlayerDetector
 @onready var area_attack: ShapeCast3D = $Rig/AreaAttack
+@onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
 
 # Finds the player in the scene.
 @onready var player: Player = get_tree().get_first_node_in_group("Player")
@@ -19,14 +20,33 @@ func _ready() -> void:
 	health_component.update_max_health(max_health)
 
 func _physics_process(delta: float) -> void:
+	var velocity_target := Vector3.ZERO
+	navigation_agent.target_position = player.global_position
+	
 	if rig.is_idle():
 		check_for_attacks()
+		if not navigation_agent.is_target_reached():
+			velocity_target = get_local_navigation_direction() * 5.0
+			orient_rig(navigation_agent.get_next_path_position())
+	
+	navigation_agent.velocity = velocity_target
 
 func check_for_attacks() -> void:
 	for collision_id in player_detector.get_collision_count():
 		var collider = player_detector.get_collider(collision_id)
 		if collider is Player:
 			rig.travel("Overhead")
+
+func orient_rig(target_position: Vector3) -> void:
+	target_position.y = rig.global_position.y
+	if rig.global_position.is_equal_approx(target_position):
+		return
+	rig.look_at(target_position, Vector3.UP, true)
+
+func get_local_navigation_direction() -> Vector3:
+	var destination = navigation_agent.get_next_path_position()
+	var local_destination = destination - global_position
+	return local_destination.normalized()
 
 func _on_health_component_defeat() -> void:
 	player.stats.xp += xp_value
@@ -36,3 +56,8 @@ func _on_health_component_defeat() -> void:
 
 func _on_rig_heavy_attack() -> void:
 	area_attack.deal_damage(20.0, crit_rate)
+
+
+func _on_navigation_agent_3d_velocity_computed(safe_velocity: Vector3) -> void:
+	velocity = safe_velocity
+	move_and_slide()
